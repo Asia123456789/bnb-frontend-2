27638d0b983ae21ee81ef4318757f657851de19a
asia <asiia92@hotmail.com>
2025-11-03 17:02:08 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to Bnb Frontend!</h1>
+      <p>Your React + TypeScript frontend is ready.</p>
+    </div>
+  );
+};
+
+export default App;
